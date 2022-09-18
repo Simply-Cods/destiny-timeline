74ea7ab1144e2c97212b682203110e7e22d75c3c
Simply-Cods <79521176+Simply-Cods@users.noreply.github.com>
2022-09-18 23:05:26 +0200

    deleted unused code from image generator

--- image-generator.js
+++ image-generator.js
@@ -2,90 +2,6 @@ const fs = require('fs/promises');
 const fss = require('fs')
 const seasons = require('./src/data/seasons.json');
 
-// async function generate() {
-//     const genDir = `${__dirname}/src/components/image-gen`
-
-//     console.log(`Looking for ${genDir}`)
-//     if(!fss.existsSync(genDir)) {
-//         console.log(`Creating ${genDir} directory`)
-//         await fs.mkdir(genDir)
-//         console.log("Success")
-//     }
-
-//     let indexImports = "";
-//     let indexExports = "";
-//     let smartSelectorImports = "// Auto Generated\nimport React from 'react'\nimport {";
-//     let smartSelectorBody = "export interface SmartSelectorProps {season: number; className?: string}\nexport default function SmartSelector(props: SmartSelectorProps) {\nlet selected:JSX.Element = <></> \nswitch(props.season) {\n"
-
-//     for(let i = 0; i < seasons.length; i++) {
-//         const season = seasons[i];
-//         const fileNameLowercase = season.icon.split('.')[0];
-//         const fileName = fileNameLowercase[0].toUpperCase() + fileNameLowercase.slice(1).toLowerCase();
-
-//         const path = `${genDir}/${fileName}.tsx`;
-
-//         await deleteFile(path)
-        
-//         const content = `// Auto generated
-// import { StaticImage } from 'gatsby-plugin-image'
-// import React from 'react'
-
-// export interface ${fileName}Props {
-//     className?: string
-// }
-
-// export default function ${fileName}(props: ${fileName}Props) {
-//     return (
-//         <StaticImage src="${__dirname}/src/images/${season.icon}" alt="${season.title}" className={props.className}/>
-//     )
-// }`
-//         await writeFile(path, content)
-
-//         indexImports += `import ${fileName}Import from './${fileName}'; \n`
-//         indexExports += `export const ${fileName} = ${fileName}Import; \n`
-
-//         smartSelectorImports += `${fileName},\n`
-//         smartSelectorBody += `case ${season.number}:\nselected = <${fileName} className={props.className}/>\nbreak;\n`
-//     }
-
-   
-//     indexImports += "import SmartSelectorImport from './SmartSelector' \n"
-//     indexExports += "export const SmartSelector = SmartSelectorImport"
-
-//     smartSelectorImports += "} from '.'\n\n"
-//     smartSelectorBody += "}\nreturn selected\n}"
-//     const smartSelectorPath = `${genDir}/SmartSelector.tsx`
-//     const smartSelectorContent = smartSelectorImports + smartSelectorBody;
-
-//     await deleteFile(smartSelectorPath)
-
-//     await writeFile(smartSelectorPath, smartSelectorContent)
-
-//     const indexContent = "// Auto generated \n" + indexImports + '\n' + indexExports
-    
-//     const indexPath = `${genDir}/index.ts`
-
-//     await deleteFile(indexPath);
-
-//     await writeFile(indexPath, indexContent);
-
-//     async function deleteFile(path) {
-//         if(fss.existsSync(path)) {
-//             console.log(`Deleting existing ${path}`)
-//             await fs.unlink(path);
-//             console.log('Success')
-//         }
-//     }
-
-//     async function writeFile(path, content) {
-//         console.log(`Writing file ${path}`)
-//         await fs.writeFile(path, content);
-//         console.log('Success')
-//     }
-// }
-
-
-
 async function generate() {
     const genDir = `${__dirname}/src/components/image-gen`
     await setupDir();
@@ -95,9 +11,7 @@ async function generate() {
     const rootIndexContent = "// Auto generated\n" + rootIndexImport + rootIndexExport;
     await write(genDir + "/index.ts", rootIndexContent)
 
-    async function setupDir() {
-        // const genDir = `${__dirname}/src/components/image-gen`
-    
+    async function setupDir() {    
         await mkdir(genDir);
     }
     
