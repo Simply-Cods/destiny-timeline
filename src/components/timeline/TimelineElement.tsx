68b8489c3f49738108eda0c92c380da84a20647a
Simply-Cods <79521176+Simply-Cods@users.noreply.github.com>
2022-09-21 16:55:57 +0200

    moved timeline components to their own folder

--- src/components/timeline/TimelineElement.tsx
+++ src/components/timeline/TimelineElement.tsx
@@ -8,8 +8,8 @@ import {
 } from '@mui/lab'
 import * as styles from './TimelineElement.module.scss'
 import { TimelineData, Season } from "./TimelineRenderer";
-import { getLoreString } from "../helpers";
-import { SeasonIcon } from "./image-gen";
+import { getLoreString } from "../../helpers";
+import { SeasonIcon } from "../image-gen";
 
 export interface TimelineElementProps {
     timelineData: TimelineData;
@@ -35,7 +35,11 @@ export default function TimelineElement(props: TimelineElementProps) {
       <TimelineContent>
         <div>
           <>
-            <SeasonIcon season={props.timelineData.seasonAdded} className={styles.season}/>
+            <SeasonIcon 
+              season={props.timelineData.seasonAdded} 
+              className={styles.season} 
+              placeholder="tracedSVG" 
+            />
             {props.timelineData.title !== "" && <h1>{props.timelineData.title}</h1>}
             {props.timelineData.subtitle !== "" && <p>{props.timelineData.subtitle}</p>}
             <ul>
