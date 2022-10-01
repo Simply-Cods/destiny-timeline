267a690afe42734d15c7b0e4e399316e67fe00ec
Simply-Cods <79521176+Simply-Cods@users.noreply.github.com>
2022-10-02 01:12:16 +0200

    automatically launch api for the deveditor

--- destiny-timeline-deveditor/src/components/timeline/TimelineEditor.tsx
+++ destiny-timeline-deveditor/src/components/timeline/TimelineEditor.tsx
@@ -1,120 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Column from './Column';
 import { TimelineDefinition } from '../../../../src/types'
 import './TimelineEditor.scss'
 import Controls from './Controls';
 
 export default function Timeline() {
-    const [data, setData] = useState<TimelineDefinition[]>([
-        {
-            contentDirection: "",
-            debugName: "before",
-            children: [
-                {
-                    time: "Before time itself",
-                    title: "THE FLOWER GAME",
-                    subtitle: "The Gardener and Winnower cycle the pattern",
-                    seasonAdded: 16,
-                    style: "empty",
-                    sources: [
-                        "https://www.ishtar-collective.net/entries/the-flower-game#book-unveiling"
-                    ]
-                  },
-                {
-                  time: "Before time itself",
-                  title: "THE FLOWER GAME",
-                  subtitle: "The Gardener and Winnower cycle the pattern",
-                  seasonAdded: 16,
-                  style: "major",
-                  sources: [
-                      "https://www.ishtar-collective.net/entries/the-flower-game#book-unveiling"
-                  ]
-                },
-                {
-                  time: "Before time itself",
-                  title: "THE FLOWER GAME",
-                  subtitle: "The Gardener and Winnower cycle the pattern",
-                  seasonAdded: 16,
-                  style: "empty",
-                  sources: [
-                      "https://www.ishtar-collective.net/entries/the-flower-game#book-unveiling"
-                  ]
-                },
-            ],
-        },
-        {
-            contentDirection: "",
-            debugName: "default",
-            children: [
-                {
-                  time: "Before time itself",
-                  title: "THE FLOWER GAME",
-                  subtitle: "The Gardener and Winnower cycle the pattern",
-                  seasonAdded: 16,
-                  style: "major",
-                  sources: [
-                      "https://www.ishtar-collective.net/entries/the-flower-game#book-unveiling"
-                  ]
-                },
-                {
-                  time: "Before time itself",
-                  title: "THE FLOWER GAME",
-                  subtitle: "The Gardener and Winnower cycle the pattern",
-                  seasonAdded: 16,
-                  style: "major",
-                  sources: [
-                      "https://www.ishtar-collective.net/entries/the-flower-game#book-unveiling"
-                  ]
-                },
-                {
-                    time: "Before time itself",
-                    title: "THE FLOWER GAME",
-                    subtitle: "The Gardener and Winnower cycle the pattern",
-                    seasonAdded: 16,
-                    style: "major",
-                    sources: [
-                        "https://www.ishtar-collective.net/entries/the-flower-game#book-unveiling"
-                    ]
-                },
-            ],
-        },
-        {
-            contentDirection: "",
-            debugName: "after",
-            children: [
-                {
-                    time: "Before time itself",
-                    title: "THE FLOWER GAME",
-                    subtitle: "The Gardener and Winnower cycle the pattern",
-                    seasonAdded: 16,
-                    style: "empty",
-                    sources: [
-                        "https://www.ishtar-collective.net/entries/the-flower-game#book-unveiling"
-                    ]
-                  },
-                {
-                  time: "Before time itself",
-                  title: "THE FLOWER GAME",
-                  subtitle: "The Gardener and Winnower cycle the pattern",
-                  seasonAdded: 16,
-                  style: "major",
-                  sources: [
-                      "https://www.ishtar-collective.net/entries/the-flower-game#book-unveiling"
-                  ]
-                },
-                {
-                  time: "Before time itself",
-                  title: "THE FLOWER GAME",
-                  subtitle: "The Gardener and Winnower cycle the pattern",
-                  seasonAdded: 16,
-                  style: "major",
-                  sources: [
-                      "https://www.ishtar-collective.net/entries/the-flower-game#book-unveiling"
-                  ]
-                },
-            ],
-        }
-    ])
+    const [data, setData] = useState<TimelineDefinition[]>([])
+
+    useEffect(() => {
+        fetch('http://localhost:4001/timelines')
+        .then(body => body.json())
+        .then(data => {
+            console.log(data)
+            setData(data)
+        })
+    }, [])
 
     const rows = data.map((column, i) => {
         return <Column 
